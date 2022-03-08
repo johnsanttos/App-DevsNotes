6478f79d512dac06aab8d6259afaece22522cd72
John Santos <jhow.mito11@gmail.com>
2022-03-08 17:08:06 -0300

    finalizando redux

--- appDevNotes/src/pages/EditNote.js
+++ appDevNotes/src/pages/EditNote.js
@@ -1,25 +1,106 @@
-import React from 'react';
-import { View, Text, Button} from 'react-native';
+import React, { useState, useEffect, useLayoutEffect } from 'react';
+import { View, TextInput, TouchableHighlight, Image } from 'react-native';
 import { useNavigation, useRoute } from '@react-navigation/native';
 import styles from '../styles/editNoteStyles';
+import { useDispatch, useSelector } from 'react-redux';
+
 
 export default function Pages(props) {
 
-const navigation = useNavigation()
+    const navigation = useNavigation()
+    const route = useRoute()
+    const dispatch = useDispatch()
+    const list = useSelector(state => state.notes.list)
 
-const name = props.route.params?.name ?? 'Visitante';
+    const [title, setTitle] = useState('')
+    const [body, setBody] = useState('')
+    const [status, setStatus] = useState('new')
 
-function handleBackButton(){
-    navigation.goBack()
-}
 
- return (
-    <View style= {styles.container}>
-       <Text> Tela sobre: {name}</Text>
-       <Button 
-       title= " Voltar para Home"
-       onPress={handleBackButton}
-       />
-   </View>
-  );
+    useEffect(() => {
+        if (route.params?.key != undefined && list[route.params.key]) {
+            setStatus('edit')
+            setTitle(list[route.params.key].title)
+            setBody(list[route.params.key].body)
+        }
+
+    }, [])
+
+    useLayoutEffect(() => {
+        navigation.setOptions({
+            title: status == 'new' ? 'Nova Anotação' : 'Editar Anotação',
+            headerLeft: () => (
+                <TouchableHighlight
+                    style={styles.closeButton}
+                    onPress={handleClose}
+                    underlayColor="transparent">
+                    <Image
+                        style={styles.closeButtonImage}
+                        source={require('../assets/close.png')} />
+                </TouchableHighlight>
+            ),
+
+            headerRight: () => (
+                <TouchableHighlight style={styles.saveButton}
+                    underlayColor="transparent"
+                    onPress={handleSaveButton}
+                >
+                    <Image style={styles.saveButtonImage} source={require('../assets/save.png')} />
+                </TouchableHighlight>
+
+
+            )
+
+        })
+
+    }, [status, title, body])
+
+    const handleSaveButton = () => {
+        if (title != '' && body != '') {
+
+            if (status == 'edit') {
+
+            } else {
+                dispatch({
+                    type: 'ADD_NOTE',
+                    payload: {
+                        title: title,
+                        body: body
+                    } // voce pode substituir por body,title por serem iguais
+                })
+            }
+            navigation.goBack()
+        } else {
+            alert('Preencha título e corpo!')
+
+        }
+    }
+
+    const handleClose = () => {
+        navigation.goBack()
+    }
+
+
+    return (
+        <View style={styles.container}>
+            <TextInput style={styles.titleInput}
+                placeholder="Digite o titulo da anotação"
+                placeholderTextColor='#ccc'
+                value={title}
+                onChangeText={t => setTitle(t)}
+                autoFocus={true}
+            />
+
+            <TextInput style={styles.bodyInput}
+                placeholder="Digite o titulo da anotação"
+                placeholderTextColor='#ccc'
+                multiline={true}
+                textAlignVertical='top'
+                value={body}
+                onChangeText={t => setBody(t)}
+
+            />
+
+        </View>
+    );
 }
--- appDevNotes/src/reducers/NotesReducer.js
+++ appDevNotes/src/reducers/NotesReducer.js
@@ -1,17 +1,21 @@
-const initialState ={
+const initialState = {
     list: [
-        { title:'Primeira Nota', body:'Testando 1,2,3...' }
+        { title: 'Primeira Nota', body: 'Testando 1,2,3...' }
     ]
 }
 
 
 export default (state = initialState, action) => {
-switch(action.type){
-    case'ADD_NOTE':
+    let newList = [...state.list] // copia de state.list para o redux para corrigir bug redux
+    switch (action.type) {
+        case 'ADD_NOTE':
+            newList.push({
+                title: action.payload.title,
+                body: action.payload.body
+            })
+            break
+    }
 
-    break
-}
 
-
-    return state
+    return { ...state, list: newList } //ira retornar a list ou copia da list
 }
